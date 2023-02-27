4332d49ddfa1c88feffab9b710eff0b75c7e77ca
floworker <102927903+floworker@users.noreply.github.com>
2023-02-27 02:11:00 +0200

    Create space.js

---
+++ space.js
@@ -0,0 +1,50 @@
+particlesJS("space", {
+  "particles": {
+    "number": {
+      "value": 49,
+      "density": {
+        "enable": true,
+        "value_area": 500
+      }
+    },
+    "color": {
+      "value": "#86592d"
+    },
+    "opacity": {
+      "value": 1,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0.4,
+        "sync": false
+      }
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "size": {
+      "value": 5,
+      "random": true
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "right",
+      "straight": false
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true
+      },
+      "onclick": {
+        "enable": true
+      }
+    }
+  }
+});
